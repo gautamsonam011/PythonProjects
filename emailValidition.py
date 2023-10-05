cdefdfbdb65bac8002439781ae1f9110a89eae3c
Sonam Gautam <121800288+gautamsonam011@users.noreply.github.com>
2023-10-05 12:25:56 +0530

    Add files via upload

---
+++ emailValidition.py
@@ -0,0 +1,38 @@
+# Check Validate E-Mail Address in Python 
+# (Using String Function)
+
+email = input("Enter your email:") #g@g.in,hfdh@gmail.com
+k = 0
+j = 0
+d = 0
+if len(email) >= 6:
+    if email[0].isalpha():
+        if ("@" in email) and (email.count("@") == 1):
+            if (email[-4] == ".") ^ (email[-3] == "."):
+                for i in email:
+                    if i == i.isspace():
+                        k = 1
+                    elif i.isalpha():
+                        if i == i.upper(): # small like-> w->W or W==W
+                            j = 1
+                    elif i.isdigit():
+                        continue
+                    elif i == "_" or i == "." or i == "@":
+                        continue
+                    else:
+                        d = 1       
+                if k == 1 or j == 1 or d == 1:
+                    print("Wrong email 5")
+                    
+            else:
+                print("Wrong Email 4")
+        else:
+            print("@ more than one 3")
+            
+    else:
+        print("Invalid Email! 2")
+else:
+    print("Wrong Email 1")
+    
+    
+    
